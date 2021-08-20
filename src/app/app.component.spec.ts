1518e2b5ba4ba98cb7e2490f6bf573d6b569b8c1
lindam-code <linda.minotti2@gmail.com>
2021-08-20 13:10:47 +0200

    template griglia

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'e-commerce'`, () => {
+  it(`should have as title 'tic-tac-toe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('e-commerce');
+    expect(app.title).toEqual('tic-tac-toe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('e-commerce app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('tic-tac-toe app is running!');
   });
 });
